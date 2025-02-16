a8b00efd4d4b0f54bf984b0affaa3d69c74b49b3
unknown <elmurodvkh@gmail.com>
2025-02-16 23:33:28 +0500

    auth ui #1

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import { ReactNode } from 'react'
+
+export default function layout({ children }: { children: ReactNode }) {
+    return (
+        <main className='auth-containder'>
+            <section className='auth-form'>
+                <div className='auth-box'>
+                    <div className='flex flex-row gap-3'>
+                        <Image
+                            src='/icons/logo.svg'
+                            alt='logo'
+                            width={37}
+                            height={37}
+                        />
+                        <h1 className='text-2xl font-semibold text-white'>
+                            BookWise
+                        </h1>
+                    </div>
+
+                    <div>
+                        {children}
+                    </div>
+                </div>
+            </section>
+
+            <section className='auth-illustration'>
+                <Image
+                    src='/images/auth-illustration.png'
+                    alt='illustration'
+                    width={1000}
+                    height={1000}
+                    className='size-full object-cover'
+                />
+            </section>
+        </main>
+    )
+}
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,17 @@
+import AuthForm from "@/components/AuthForm";
+import { signUpSchema } from "@/lib/validations";
+
+export default function page() {
+    return <AuthForm
+        type='SIGN_UP'
+        schema={signUpSchema}
+        defaultValues={{
+            email: "",
+            password: "",
+            fullName: "",
+            universityId: "",
+            universityCard: "",
+        }}
+        onSubmit={() => { }}
+    />
+}
